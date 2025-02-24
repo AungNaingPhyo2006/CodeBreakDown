01075ce27767056f8b706e5318f4a5af6f63db73
AungNaingPhyo2006 <115768795+AungNaingPhyo2006@users.noreply.github.com>
2025-02-24 23:16:24 +0630

    Update index.jsx

--- src/codeBreakDown/index.jsx
+++ src/codeBreakDown/index.jsx
@@ -3,61 +3,42 @@ import React, { useEffect, useState } from 'react';
 import { data } from '../model';
 
 const index = () => {
-  const [updatedList, setUpdatedList]= useState([])
-
+ const [updatedList , setUpdatedList] = useState([])
   useEffect(() => {
     getData()
   }, [])
 
-  console.log("Data =>", data)
 
   const getData = () => {
-      if (Array.isArray(data)) {
+    if(Array.isArray(data)){
       const updatedItems = data.map((item) => {
-          if (item.ApproverDetail) {
-              const approverDetails = item.ApproverDetail.split(
-                  '||',
-              ).map((detail) => {
-                  const [name, status, action] =
-                      detail.split(' $$ ');
-                  let color = 'gold'; 
-                  switch (action.trim().toLowerCase()) {
-                      case 'pending':
-                          color = 'blue';
-                          break;
-                      case 'approved':
-                          color = 'green';
-                          break;
-                      case 'rejected':
-                          color = 'red';
-                          break;
-                      case 'inaction':
-                          color = 'gold';
-                          break;
-                      default:
-                          color = 'gold';
-                  }
-                  return {
-                      ApproverName: name.trim(),
-                      ApproverComment: action.trim(),
-                      ApproverStatus: status.trim(),
-                      color: color,
-                  };
-              });
+        if(item.ApproverDetail) {
+          const approverDetails = item.ApproverDetail?.split('||').map((detail)=>{
+            const [name , status , action] = detail.split(' $$ ')
+            return {
+              ApproverName : name.trim(),
+              ApproverStatus: status.trim(),
+              ApproverAction : action.trim()
+            }
+          })
+        return {
+          ...item,
+          ApproverDetail : approverDetails
+        }
+        }else {
+          return item;
+        }
+     
+      })   
+      setUpdatedList(updatedItems)
+    }else{
+      console.log("No Array format!")
+    }
+ 
+  }
 
-              return {
-                  ...item,
-                  ApproverDetail: approverDetails,
-              };
-          } else {
-              return item;
-          }
-      });
-      setUpdatedList(updatedItems);
-  } else {
-       console.log('Unexpected response format:', data);
-  }
-  }
+  console.log('UpdatedItems=>', updatedList)
+
   return (
     <View style={styles.container}>
       <Text style={styles.txtStyle}>Code Breakdown</Text>
