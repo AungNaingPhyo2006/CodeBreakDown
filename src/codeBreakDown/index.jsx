669453bc574ce50023278d803f7140c04e5902ad
AungNaingPhyo2006 <115768795+AungNaingPhyo2006@users.noreply.github.com>
2025-04-24 20:11:20 +0630

    Update index.jsx

--- src/codeBreakDown/index.jsx
+++ src/codeBreakDown/index.jsx
@@ -3,45 +3,25 @@ import React, { useEffect, useState } from 'react';
 import { data } from '../model';
 
 const index = () => {
- const [updatedList , setUpdatedList] = useState([])
-  useEffect(() => {
-    getData()
-  }, [])
-
-
-  const getData = () => {
-    if(Array.isArray(data)){
-      const updatedItems = data.map((item) => {
-        if(item.ApproverDetail) {
-          const approverDetails = item.ApproverDetail?.split('||').map((detail)=>{
-            const [name , status , action] = detail.split(' $$ ')
-            return {
-              ApproverName : name.trim(),
-              ApproverStatus: status.trim(),
-              ApproverAction : action.trim()
-            }
-          })
-        return {
-          ...item,
-          ApproverDetail : approverDetails
-        }
-        }else {
-          return item;
-        }
-     
-      })   
-      setUpdatedList(updatedItems)
-    }else{
-      console.log("No Array format!")
-    }
- 
-  }
-
-  console.log('UpdatedItems=>', updatedList)
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.txtStyle}>Code Breakdown</Text>
+    <View style={{flex:1, backgroundColor: 'blue'}}>
+
+     <View style={{ height:100 , backgroundColor:'cyan'}}>
+      <View style={{flexDirection:'row'}}>
+       <Text style={{color:'blue'}}>Header</Text>
+       <Text style={{color:'blue'}}>Title</Text>
+      </View>
+     </View>
+
+     <View style={{flex:1, backgroundColor:'red'}}>
+      <Text style={{color:'white'}}>Content</Text>
+     </View>
+
+     <View style={{ height: 100, backgroundColor:'green'}}>
+      <Text style={{color:'white'}}>Footer</Text>
+     </View> 
+
     </View>
   );
 };
